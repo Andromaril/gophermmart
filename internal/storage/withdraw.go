ee540f49dd25706d4f5527bbd9373ad0defd0c90
Andronik Maria <andronikmar@yandex.com>
2024-04-22 00:13:02 +0300

    add withdrawal4

--- internal/storage/withdraw.go
+++ internal/storage/withdraw.go
@@ -17,7 +17,7 @@ func (m *Storage) GetWithdrawal(login string) ([]model.Withdrawn, error) {
 	rows, err := m.DB.QueryContext(m.Ctx, "SELECT orders.number, sum, processed_at FROM withdrawals INNER JOIN orders ON withdrawals.order_id = order_id WHERE login=$1", login)
 	//err := rows.Scan(&value)
 	if err != nil {
-		log.Println("This is a log message!")
+		log.Printf("%q", err)
 		return result, fmt.Errorf("invalid login %q", err)
 	}
 
