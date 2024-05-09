255d2d2b76ed26e94ce8f64953ce1e7a2adaca28
Andronik Maria <andronikmar@yandex.com>
2024-05-09 19:24:12 +0300

    new ref3

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,62 +1,50 @@
 package main
 
 import (
-	"context"
-	"database/sql"
-	"net/http"
-	"time"
-
-	"github.com/go-chi/chi"
-
 	_ "github.com/jackc/pgx/v5/stdlib"
 
-	"github.com/andromaril/gophermmart/internal/accrual"
-	"github.com/andromaril/gophermmart/internal/errormart"
 	"github.com/andromaril/gophermmart/internal/flag"
-	h "github.com/andromaril/gophermmart/internal/handler"
-	"github.com/andromaril/gophermmart/internal/middleware"
-	storagedb "github.com/andromaril/gophermmart/internal/storage"
-	log "github.com/sirupsen/logrus"
+	"github.com/andromaril/gophermmart/internal/start"
 )
 
-func Update(newdb *storagedb.Storage) {
-	for {
-		log.Info("start send request to accrual")
-		err := accrual.Accrual(newdb)
+// func Update(newdb *storagedb.Storage) {
+// 	for {
+// 		log.Info("start send request to accrual")
+// 		err := accrual.Accrual(newdb)
 
-		if err != nil {
-			e := errormart.NewMartError(err)
-			log.Error("error witg accraul ", e.Error())
-		}
-		time.Sleep(time.Second * 5)
-	}
-}
+// 		if err != nil {
+// 			e := errormart.NewMartError(err)
+// 			log.Error("error witg accraul ", e.Error())
+// 		}
+// 		time.Sleep(time.Second * 5)
+// 	}
+// }
 
-func NewRouter(newdb storagedb.Storage) {
-	r := chi.NewRouter()
-	r.Post("/api/user/register", h.Register(newdb))
-	r.Post("/api/user/login", h.Login(newdb))
-	r.With(middleware.AuthMiddleware).Post("/api/user/orders", h.NewOrder(newdb))
-	r.With(middleware.AuthMiddleware).Get("/api/user/orders", h.GetOrder(newdb))
-	r.With(middleware.AuthMiddleware).Get("/api/user/balance", h.GetBalance(newdb))
-	r.With(middleware.AuthMiddleware).Post("/api/user/balance/withdraw", h.NewWithdrawal(newdb))
-	r.With(middleware.AuthMiddleware).Get("/api/user/withdrawals", h.GetWithdrawal(newdb))
-	if err := http.ListenAndServe(flag.FlagRunAddr, r); err != nil {
-		panic(err)
+// func NewRouter(newdb storagedb.Storage) {
+// 	r := chi.NewRouter()
+// 	r.Post("/api/user/register", h.Register(newdb))
+// 	r.Post("/api/user/login", h.Login(newdb))
+// 	r.With(middleware.AuthMiddleware).Post("/api/user/orders", h.NewOrder(newdb))
+// 	r.With(middleware.AuthMiddleware).Get("/api/user/orders", h.GetOrder(newdb))
+// 	r.With(middleware.AuthMiddleware).Get("/api/user/balance", h.GetBalance(newdb))
+// 	r.With(middleware.AuthMiddleware).Post("/api/user/balance/withdraw", h.NewWithdrawal(newdb))
+// 	r.With(middleware.AuthMiddleware).Get("/api/user/withdrawals", h.GetWithdrawal(newdb))
+// 	if err := http.ListenAndServe(flag.FlagRunAddr, r); err != nil {
+// 		panic(err)
 
-	}
-	//return r
-}
-func Start() (*sql.DB, storagedb.Storage) {
-	var err error
-	var newdb storagedb.Storage
-	var db *sql.DB
-	db, err = newdb.Init(flag.Databaseflag, context.Background())
-	if err != nil {
-		panic(err)
-	}
-	return db, newdb
-}
+// 	}
+// 	//return r
+// }
+// func Start() (*sql.DB, storagedb.Storage) {
+// 	var err error
+// 	var newdb storagedb.Storage
+// 	var db *sql.DB
+// 	db, err = newdb.Init(flag.Databaseflag, context.Background())
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	return db, newdb
+// }
 
 func main() {
 	//var err error
@@ -67,9 +55,9 @@ func main() {
 	// if err != nil {
 	// 	panic(err)
 	// }
-	db, newdb := Start()
+	db, newdb := start.Start()
 	defer db.Close()
-	go Update(&newdb)
+	go start.Update(&newdb)
 	// r := chi.NewRouter()
 	// r.Post("/api/user/register", h.Register(newdb))
 	// r.Post("/api/user/login", h.Login(newdb))
@@ -78,7 +66,7 @@ func main() {
 	// r.With(middleware.AuthMiddleware).Get("/api/user/balance", h.GetBalance(newdb))
 	// r.With(middleware.AuthMiddleware).Post("/api/user/balance/withdraw", h.NewWithdrawal(newdb))
 	// r.With(middleware.AuthMiddleware).Get("/api/user/withdrawals", h.GetWithdrawal(newdb))
-	NewRouter(newdb)
+	start.NewRouter(newdb)
 	// if err := http.ListenAndServe(flag.FlagRunAddr, r); err != nil {
 	// 	panic(err)
 
---
+++ internal/start/start.go
@@ -0,0 +1,56 @@
+package start
+
+import (
+	"context"
+	"database/sql"
+	"net/http"
+	"time"
+
+	"github.com/andromaril/gophermmart/internal/accrual"
+	"github.com/andromaril/gophermmart/internal/errormart"
+	"github.com/andromaril/gophermmart/internal/flag"
+	h "github.com/andromaril/gophermmart/internal/handler"
+	"github.com/andromaril/gophermmart/internal/middleware"
+	storagedb "github.com/andromaril/gophermmart/internal/storage"
+	"github.com/go-chi/chi"
+	log "github.com/sirupsen/logrus"
+)
+
+func Update(newdb *storagedb.Storage) {
+	for {
+		log.Info("start send request to accrual")
+		err := accrual.Accrual(newdb)
+
+		if err != nil {
+			e := errormart.NewMartError(err)
+			log.Error("error witg accraul ", e.Error())
+		}
+		time.Sleep(time.Second * 5)
+	}
+}
+
+func NewRouter(newdb storagedb.Storage) {
+	r := chi.NewRouter()
+	r.Post("/api/user/register", h.Register(newdb))
+	r.Post("/api/user/login", h.Login(newdb))
+	r.With(middleware.AuthMiddleware).Post("/api/user/orders", h.NewOrder(newdb))
+	r.With(middleware.AuthMiddleware).Get("/api/user/orders", h.GetOrder(newdb))
+	r.With(middleware.AuthMiddleware).Get("/api/user/balance", h.GetBalance(newdb))
+	r.With(middleware.AuthMiddleware).Post("/api/user/balance/withdraw", h.NewWithdrawal(newdb))
+	r.With(middleware.AuthMiddleware).Get("/api/user/withdrawals", h.GetWithdrawal(newdb))
+	if err := http.ListenAndServe(flag.FlagRunAddr, r); err != nil {
+		panic(err)
+
+	}
+	//return r
+}
+func Start() (*sql.DB, storagedb.Storage) {
+	var err error
+	var newdb storagedb.Storage
+	var db *sql.DB
+	db, err = newdb.Init(flag.Databaseflag, context.Background())
+	if err != nil {
+		panic(err)
+	}
+	return db, newdb
+}
